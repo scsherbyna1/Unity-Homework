3e29996e043e304d1613f3f7c2a2d8d08e7aa1d7
andreywh <Pereadrey@ukr.net>
2020-06-03 21:45:00 +0300

    updated
    updated

---
+++ Assets/Scripts/PauseController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseController : MonoBehaviour
+{
+    public GameObject pausePanel;
+    public void PauseGame()
+    {
+        pausePanel.SetActive(true);
+        Time.timeScale = 0f;
+
+    }
+    public void ResumeGame()
+    {
+        pausePanel.SetActive(false);
+        Time.timeScale = 1f;
+    }
+   public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+        Time.timeScale = 1f;
+    }
+    
+}
